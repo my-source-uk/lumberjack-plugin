16c04044f07f7d51fb24bdf93d242fb2fcdb8164
Richard Browne <r.a.browne@gmail.com>
2021-03-09 16:40:22 +0000

    ⚾️ cover the bases.

--- public/js/ping.js
+++ public/js/ping.js
@@ -1,9 +1,23 @@
+var hasLumberjackSaidBye = false;
+var lumberjackBye = function() {
+    if (!hasLumberjackSaidBye) return true;
+    navigator.sendBeacon('/lumberjack/bye');
+    var until = new Date()
+        .getTime() + 100;
+    while (new Date()
+        .getTime() < until);
+    hasLumberjackSaidBye = true;
+};
 document.addEventListener('visibilitychange', function logData() {
     if (document.visibilityState === 'hidden') {
-        navigator.sendBeacon('/lumberjack/bye');
-        var until = new Date()
-            .getTime() + 100;
-        while (new Date()
-            .getTime() < until);
+        lumberjackBye();
+    } else {
+        hasLumberjackSaidBye = false;
     }
+});
+window.onbeforeunload(function() {
+    lumberjackBye();
+});
+window.onunload(function() {
+    lumberjackBye();
 });
